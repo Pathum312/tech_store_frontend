fede6c2ceccb08c439f0cf5d68f703b578c78ee1
Pathum312 <pathum.2019557@iit.ac.lk>
2024-03-15 23:03:24 +0530

    c

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -3,7 +3,13 @@
 </script>
 
 <div
-	class="w-sidebar hover:w-sidebar-hover hover:block transition-all ease-in-out delay-200 bg-primary"
+	class="w-sidebar hover:w-sidebar-hover hover:block transition-all ease-in-out delay-200 bg-primary test"
 >
 	<p>Test</p>
 </div>
+
+<style>
+	.test {
+		color: var(--text-primary);
+	}
+</style>
